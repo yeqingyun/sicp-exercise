248e6a90a3f683ef302ded54daf989635890be93
yeqingyun <yeqy@gionee.com>
2018-01-31 18:21:42 +0800

    Signed-off-by: yeqingyun <yeqy@gionee.com>

--- chapter 2.scm
+++ chapter 2.scm
@@ -71,17 +71,17 @@
 
 ;; 2.5
 
-(define (cons a b)
-  (* (expt 2 a) (expt 3 b)))
+;; (define (cons a b)
+;;   (* (expt 2 a) (expt 3 b)))
 
-(define (car z)
-  (if (= (remainder z 2) 0)
-	  (+ 1 (car (/ z 2)))
-	  0))
-(define (cdr z)
-  (if (= (remainder z 3) 0)
-	  (+ 1 (cdr (/ z 3)))
-	  0))
+;; (define (car z)
+;;   (if (= (remainder z 2) 0)
+;; 	  (+ 1 (car (/ z 2)))
+;; 	  0))
+;; (define (cdr z)
+;;   (if (= (remainder z 3) 0)
+;; 	  (+ 1 (cdr (/ z 3)))
+;; 	  0))
 
 ;; 2.6
 
@@ -121,3 +121,6 @@
     (lambda (f)
         (lambda (x)
             (f x))))
+
+
+
