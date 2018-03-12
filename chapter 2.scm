55559ba5685ce86d6b529ea9ca9645a8492fbf86
yeqingyun <yeqy@gionee.com>
2018-03-12 09:02:04 +0800

    Signed-off-by: yeqingyun <yeqy@gionee.com>

--- chapter 2.scm
+++ chapter 2.scm
@@ -138,8 +138,7 @@
 		(reverse r)
 		(if (p (car t))
 			(iter p (cdr t) (cons (car t) r))
-			(iter p (cdr t) r)
-			)))
+			(iter p (cdr t) r))))
   (iter f tab '()))
 
 (define (deep-reverse t)
@@ -164,6 +163,22 @@
 			 (* x x)))
 	   tree))
 
+(define (count-leaves tree)
+  (if (null? tree)
+	  0
+	  (if (pair? tree)
+		  (+ (count-leaves (car tree))
+			 (count-leaves (cdr tree)))
+		  1)))
+
+(define (enumerate-tree tree)
+  (if (null? tree)
+	  '()
+	  (if (pair? tree)
+		  (append (enumerate-tree (car tree))
+				  (enumerate-tree (cdr tree)))
+		  (list tree))))
+
 
 (define (square-tree tree)
   (define (square x)
@@ -173,3 +188,79 @@
 		  ((not (pair? (car t))) (cons (f (car t)) (tree-map f (cdr t))))
 		  (else (cons (tree-map f (car t)) (tree-map f (cdr t))))))
   (tree-map square tree))
+
+
+(define (char-in c . ls)
+  (let loop((ls0 ls))
+	(if (null? ls0)
+		#f
+		(or (char=? c (car ls0))
+			(loop (cdr ls0))))))
+
+
+(define (enumerate-interval s n)
+  (if (> s n)
+	  '()
+	  (cons s (enumerate-interval (+ s 1) n))))
+
+
+(define t (map 
+	(lambda (i) 
+	  (map 
+	   (lambda (j) (list i j))
+	   (enumerate-interval 1 (- i 1))))
+	(enumerate-interval 1 5))) 
+
+
+;; 2.41
+(define (possible-value-of n s)
+  (define a (map
+	  (lambda (i)
+		(map 
+		 (lambda (j)
+		   (map 
+			(lambda (k) (list i j k))
+			(enumerate-interval 0 n)))
+		 (enumerate-interval 0 n)))
+	  (enumerate-interval 0 n)))
+  a)
+
+;;2.42
+(define (adjoin-position new-row k rest-of-queens)
+    (cons new-row rest-of-queens))
+
+(define (safe? k position)
+    (iter-check (car position) 
+                (cdr position)
+                 1))
+
+(define (iter-check row-of-new-queen rest-of-queens i)
+    (if (null? rest-of-queens)  ; 下方所有皇后检查完毕，新皇后安全
+        #t
+        (let ((row-of-current-queen (car rest-of-queens)))
+            (if (or (= row-of-new-queen row-of-current-queen)           ; 行碰撞
+                    (= row-of-new-queen (+ i row-of-current-queen))     ; 右下方碰撞
+                    (= row-of-new-queen (- row-of-current-queen i)))    ; 左下方碰撞
+                #f
+                (iter-check row-of-new-queen 
+                            (cdr rest-of-queens)    ; 继续检查剩余的皇后
+                            (+ i 1))))))            ; 更新步进值
+
+
+;; 2.44
+(define (up-split painter n)
+  (if (= n 0)
+	  painter
+	  (let ((smaller (up-split painter (- n 1))))
+		(below painter (beside smaller smaller)))))
+
+
+(define (cont-frac n d k)
+  (define (iter m t)
+	(if (= 1 m)
+		(/ (n m) (iter (+ 1 m) t))
+		(if (> m t)
+			1
+			(+ (d m) (/ (n m) (iter (+ 1 m) t))))))
+  (iter 1 k))
+
